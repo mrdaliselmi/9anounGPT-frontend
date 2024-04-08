2d827d650eecb727e51f0eaa4496472144c2185e
MohamedAliSelmi <mohamedali.selmi@insat.u-carthage.tn>
2024-04-08 20:40:30 +0100

    ➕ chore(deps): setup i18n and socket.io

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import './libs/i18n/i18n.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
