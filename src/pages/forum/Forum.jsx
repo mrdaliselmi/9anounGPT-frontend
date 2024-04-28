316326986b5ef3c60938e5c6d67570eb6fb6b3b9
MohamedAliSelmi <mohamedali.selmi@insat.u-carthage.tn>
2024-04-28 15:26:46 +0100

    ✨ feat: add forum navbar

--- src/pages/forum/Forum.jsx
+++ src/pages/forum/Forum.jsx
@@ -1,9 +1,10 @@
 import { Outlet } from 'react-router-dom';
+import Navbar from '@/components/forum/Navbar';
 
 function Forum() {
   return (
-    <div>
-      Forum
+    <div className="pt-[80px]">
+      <Navbar className="overflow-hidden fixed top-0 left-0 z-10" />
       <Outlet />
     </div>
   );
