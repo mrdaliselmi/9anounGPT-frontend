28131532421b6148ec084222a0051393161cff6e
MohamedAliSelmi <mohamedali.selmi@insat.u-carthage.tn>
2024-04-30 00:17:17 +0100

    🔥 fix: remove profile drop component

--- src/components/chat/NavChat.jsx
+++ src/components/chat/NavChat.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import ProfileDrop from './ProfileDrop';
+import Header from '../shared/header';
 
 function NavChat() {
   return (
     <nav className="flex items-center justify-end py-4 px-8 bg-zinc-100 w-full h-20">
       <div className="flex justify-between">
-        <ProfileDrop />
+        <Header />
       </div>
     </nav>
   );
